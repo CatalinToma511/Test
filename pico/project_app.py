2ca48b81c7ebdc9b3a28cc7b5ee26d94689db647
CatalinToma511 <catalintoma511@gmail.com>
2025-06-15 16:40:40 +0300

    async test

--- pico/project_app.py
+++ pico/project_app.py
@@ -1,2 +1,24 @@
+from ble_central import BLE_Central
+import asyncio
+import time
+
+async def task1():
+    while True:
+        time.sleep(4)
+        print("t1")
+
+async def task2():
+    while True:
+        time.sleep(2)
+        print("t2")
+
+async def main_task():
+    conn = BLE_Central()
+    tasks = [
+        asyncio.create_task(task1()),
+        asyncio.create_task(task2())
+    ]
+    await asyncio.gather(*tasks)
+
 def run():
-  print("It's working")
+    asyncio.run(main_task())
